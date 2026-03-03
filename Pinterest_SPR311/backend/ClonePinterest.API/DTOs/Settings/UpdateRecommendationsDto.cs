85ae136bdf66275e4fd1b2ae8d54735c4683f74b
Dima0913 <153444026+Dima0913@users.noreply.github.com>
2026-03-03 17:52:01 +0200

    Delete Pinterest_SPR311 directory

--- Pinterest_SPR311/backend/ClonePinterest.API/DTOs/Settings/UpdateRecommendationsDto.cs
+++
@@ -1,7 +0,0 @@
-namespace ClonePinterest.API.DTOs.Settings;
-
-public class UpdateRecommendationsDto
-{
-    public bool? RecommendationsEnabled { get; set; }
-    public bool? PersonalizedAds { get; set; }
-}
